3c1196c64f71d5a310809c7ac07e67cd7ac0315a
Rishika Arora <rishikarora2006@gmail.com>
2026-02-14 12:11:57 +0530

    delete put apis implemented with router

--- user_management/controllers/controller.js
+++ user_management/controllers/controller.js
@@ -34,3 +34,36 @@ exports.findAll = async(req,res)=>{
         res.status(404).json({message: error.message});
     }
 };
+
+exports.deleteByEmail=async (req,res)=>{
+    try{
+        const email= req.params.email;
+        const deletedUser= await UserModel.findOneAndDelete({'email':req.params.email});
+        if(!deletedUser){
+            return res.send("user not found");
+        }
+        else{
+            return res.json({"message":"User deleted successfully",deletedUser});
+        }
+
+    }
+    catch(error){
+        res.send(404).json({message: error.message});
+    }
+};
+
+exports.updatebyEmail = async (req,res)=>{
+    try{
+        const email= req.params.email;
+        const updated= await UserModel.findOneAndUpdate({'email':req.params.email},{ 'firstName':req.body.firstName, 'lastName': req.body.lastName, 'phone': req.body.phone},{new:true});
+        if(!updated){
+            return res.send(404).json({'message': 'record not found'});
+        }
+        else{
+            return res.json({'message':'record updated', updated});
+        }
+    }
+    catch(error){
+        res.json({'message': error.message});
+    }
+};
--- user_management/routes/routes.js
+++ user_management/routes/routes.js
@@ -3,4 +3,6 @@ const UserController= require('../controllers/controller')
 const router= express.Router();
 router.get('/', UserController.findAll);
 router.post('/', UserController.create);
+router.put('/:email',UserController.updatebyEmail);
+router.delete('/:email', UserController.deleteByEmail);
 module.exports=router;
